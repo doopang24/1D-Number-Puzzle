8e43f97ac85498040b17302b17251d752cca9c2e
wodus4145@naver.com <wodus4145@naver.com>
2024-11-18 22:10:32 +0900

    feat: main() 메서드 작성

--- src/NumberGame/Application.java
+++ src/NumberGame/Application.java
@@ -2,6 +2,18 @@ package NumberGame;
 
 public class Application {
     public static void main(String[] args) {
+        Application application = new Application();
 
+        System.out.println("간단 숫자 퍼즐");
+        int turn = 1;
+        System.out.println("Turn " + turn);
+        int[] numbers = new int[8];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        }
+        int[] puzzle = application.shuffle(numbers);
+        application.printPuzzle(puzzle);
+
+        int[] exchangeNumbers = application.getNumbersToSwap();
     }
 }
